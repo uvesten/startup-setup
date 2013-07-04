c1e32f22a2690c876838303695b656a02ff721b4
Petter Uvesten <uvesten@everichon.com>
2013-07-04 16:28:46 +0200

    Removed the coursera dotfiles

--- setup.sh
+++ setup.sh
@@ -51,17 +51,18 @@ ln -sb .vim/.vimrc
 ln -sb .vim/.screenrc
 
 # git pull and install dotfiles as well
-cd $HOME
-if [ -d ./dotfiles/ ]; then
-    mv dotfiles dotfiles.old
-fi
-if [ -d .emacs.d/ ]; then
-    mv .emacs.d .emacs.d~
-fi
-git clone https://github.com/startup-class/dotfiles.git
+# Defaults are fine for me atm. 
+#cd $HOME
+#if [ -d ./dotfiles/ ]; then
+    #mv dotfiles dotfiles.old
+#fi
+#if [ -d .emacs.d/ ]; then
+    #mv .emacs.d .emacs.d~
+#fi
+#git clone https://github.com/startup-class/dotfiles.git
 #ln -sb dotfiles/.screenrc .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
+#ln -sb dotfiles/.bash_profile .
+#ln -sb dotfiles/.bashrc .
+#ln -sb dotfiles/.bashrc_custom .
+#ln -sf dotfiles/.emacs.d .
 
