50756dc7e8977107c8ffeaa47756edd7097e1dc8
Petter Uvesten <uvesten@everichon.com>
2013-07-04 13:24:06 +0200

    Added heroku install to setup script

--- setup.sh
+++ setup.sh
@@ -5,6 +5,16 @@
 # Install nvm: node-version manager
 # https://github.com/creationix/nvm
 sudo apt-get install -y git-core
+
+# install heroku
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+# For more heroku fix, do
+# $ heroku login
+#$ ssh-keygen -t rsa
+#$ heroku keys:add
+
+# And in repo: heroku create; git push heroku master
+
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
